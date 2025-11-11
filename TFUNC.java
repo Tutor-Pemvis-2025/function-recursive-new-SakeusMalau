411c3694a324df67654bd13aa8a3d5851e61c794
Sakeus Malau <samtomalau8@gmail.com>
2025-11-11 21:16:10 +0700

    SAKEUS
    SELESAI

--- TFUNC.java
+++ TFUNC.java
@@ -2,19 +2,20 @@
 
 import java.util.*;
 import java.lang.Math;
+
 public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int x, hasil;
+        int x, hasil, r;
 
         x = Integer.parseInt(input.nextLine());
         System.out.println("Bilangan yang dimasukkan : " + x);
-        angka(x);
-        System.out.println("Hasil perhitungan rekursif : " + angka(x));
+        r = angka(x);
+        System.out.println("Hasil perhitungan rekursif : " + r);
     }
     
-    public static void angka(int x) {
+    public static int angka(int x) {
         int result, hasilb;
 
         if (x == 1) {
@@ -23,5 +24,7 @@ public class TFUNC {
             hasilb = angka(x - 1);
             result = x + hasilb;
         }
+        
+        return result;
     }
 }
